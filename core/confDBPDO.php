ca148466958defedfaea5d7ffaa12b905a24b47b
Luis Ferreras <luis.fergon.2@educa.jcyl.es>
2024-11-29 12:41:15 +0100

    29/11 	Configuración PDO

--- core/confDBPDO.php
+++ core/confDBPDO.php
@@ -1,9 +1,13 @@
 <?php
-//    Datos en clase
+//    Datos en entorno de explotación
+//    define("SERVIDOR", "mysql:host=localhost:3306;dbname=DB208DWESProyectoTema5");
+//    define("USUARIO", "user208DWESProyectoTema5");
+//    define("CONTRASENA", "paso1234321osap");
+//    Datos en entorno desarrollo de clase
     define("SERVIDOR", "mysql:host=daw208.isauces.local;dbname=DB208DWESProyectoTema5");
     define("USUARIO", "user208DWESProyectoTema5");
     define("CONTRASENA", "paso");
-//    Datos en casa
+//    Datos en entorno desarrollo de casa
 //    define("SERVIDOR", "mysql:host=192.168.0.208;dbname=DB208DWESProyectoTema5");
 //    define("USUARIO", "user208DWESProyectoTema5");
 //    define("CONTRASENA", "paso");
